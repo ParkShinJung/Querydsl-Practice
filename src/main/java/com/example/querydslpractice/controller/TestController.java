4ae2521da309357cba919a856389cb49916f6cc7
sjpark <sjpark@12cm.co.kr>
2023-11-22 16:12:14 +0900

    Initial commit

---
+++ src/main/java/com/example/querydslpractice/controller/TestController.java
@@ -0,0 +1,18 @@
+package com.example.querydslpractice.controller;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RequiredArgsConstructor
+@RestController
+public class TestController {
+
+    @Operation(summary = "test hello", description = "hello api example")
+    @GetMapping("/hello")
+    public ResponseEntity<String> hello(@Parameter(description = "이름", required = true, example = "Park") @RequestParam String name) {
+        return ResponseEntity.ok("hello " + name);
+    }
+}
